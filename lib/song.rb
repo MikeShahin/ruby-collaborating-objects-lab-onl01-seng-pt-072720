7d11bd8236487ada25a21d8de54c9cbaf8df1ce8
Mike Shahindoust <gobias101@gmail.com>
2020-08-21 22:11:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,4 +14,17 @@ class Song
   def save  
     @@all << self
   end
+  
+  def artist_name
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+  
+  def new_by_filename(name)
+    artist, song = name.split(" - ")
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
+  
 end
