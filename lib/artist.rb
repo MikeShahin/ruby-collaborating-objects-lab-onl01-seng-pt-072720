6ebcceeae48eea938ad9b381a547a28d53fec6cb
Mike Shahindoust <gobias101@gmail.com>
2020-08-21 22:04:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,4 +27,8 @@ class Artist
   def self.find_or_create_by_name(name)
     self.find(name) ? self.find(name) : self.new(name)
   end
+  
+  def self.find(name)
+    self.all.find {|artist| artist.name == name}
+  end
 end
